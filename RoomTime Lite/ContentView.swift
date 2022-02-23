20eae5c7678a01596ac9fae5eb8bf12bd4434c37
Talaxy <siwei.chen@shanbay.com>
2022-02-23 10:55:50 +0800

    Initial Commit

---
+++ RoomTime Lite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RoomTime Lite
+//
+//  Created by Talaxy on 2022/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RoomTime Lite/RoomTime_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomTime_LiteApp.swift
+//  RoomTime Lite
+//
+//  Created by Talaxy on 2022/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RoomTime_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
